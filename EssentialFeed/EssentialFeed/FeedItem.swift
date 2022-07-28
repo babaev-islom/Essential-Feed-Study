1e3fab3a81f4233b46d1e27344f7c4b286afab9d
babaev-islom <babaev.iislom@gmail.com>
2022-07-28 19:46:47 +0500

    Create the `FeedItem` model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Islom Babaev on 28/07/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
