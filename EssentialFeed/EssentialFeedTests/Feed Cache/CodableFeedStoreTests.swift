86dbf47447b6987d5934260e2d28c05af69245df
babaev-islom <babaev.iislom@gmail.com>
2022-08-08 17:38:17 +0500

    Move `Coddle` conformance from the framework-agnostic `LocalFeedImage` type to the new framework-specific `CodableFeedImage` type

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -11,8 +11,30 @@ import EssentialFeed
 class CodableFeedStore {
     
     private struct Cache: Codable {
-        let feed: [LocalFeedImage]
+        let feed: [CodableFeedImage]
         let timestamp: Date
+        
+        var localFeed: [LocalFeedImage] {
+            feed.map { $0.local }
+        }
+    }
+    
+    private struct CodableFeedImage: Codable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let url: URL
+        
+        init(_ image: LocalFeedImage) {
+            id = image.id
+            description = image.description
+            location = image.location
+            url = image.url
+        }
+        
+        var local: LocalFeedImage {
+            LocalFeedImage(id: id, description: description, location: location, url: url)
+        }
     }
     
     private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
@@ -23,12 +45,13 @@ class CodableFeedStore {
         }
         let decoder = JSONDecoder()
         let decoded = try! decoder.decode(Cache.self, from: data)
-        completion(.found(feed: decoded.feed, timestamp: decoded.timestamp))
+        completion(.found(feed: decoded.localFeed, timestamp: decoded.timestamp))
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
         let encoder = JSONEncoder()
-        let encoded = try! encoder.encode(Cache(feed: feed, timestamp: timestamp))
+        let cache = feed.map(CodableFeedImage.init)
+        let encoded = try! encoder.encode(Cache(feed: cache, timestamp: timestamp))
         try! encoded.write(to: storeURL)
         completion(nil)
     }
