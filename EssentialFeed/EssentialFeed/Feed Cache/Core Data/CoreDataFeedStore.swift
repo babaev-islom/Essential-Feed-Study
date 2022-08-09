83da09e7b7ec2e612aa1c15c1bc7fbcf723675da
babaev-islom <babaev.iislom@gmail.com>
2022-08-09 16:36:58 +0500

    Move `CoreDataFeedStore` to its own file in production

---
+++ EssentialFeed/EssentialFeed/Feed Cache/Core Data/CoreDataFeedStore.swift
@@ -0,0 +1,27 @@
+//
+//  CoreDataFeedStore.swift
+//  EssentialFeed
+//
+//  Created by Islom Babaev on 09/08/22.
+//
+
+import Foundation
+
+public class CoreDataFeedStore: FeedStore {
+    
+    public init() {}
+    
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+    
+    
+}
