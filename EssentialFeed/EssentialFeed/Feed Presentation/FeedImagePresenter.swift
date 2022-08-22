064f02cc232510f4c48d8b8bf6c3a639ebb24192
babaev-islom <babaev.iislom@gmail.com>
2022-08-22 13:52:41 +0500

    Move `FeedImagePresenter` into its own file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImagePresenter.swift
@@ -0,0 +1,74 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeed
+//
+//  Created by Islom Babaev on 22/08/22.
+//
+
+import Foundation
+
+public struct FeedImageViewModel<Image> {
+    public let location: String?
+    public let description: String?
+    public let image: Image?
+    public let isLoading: Bool
+    public let shouldRetry: Bool
+}
+
+public protocol FeedImageView {
+    associatedtype Image
+    
+    func display(_ viewModel: FeedImageViewModel<Image>)
+}
+
+
+public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private let imageTransformer: (Data) -> Image?
+
+    public init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+        self.view = view
+        self.imageTransformer = imageTransformer
+    }
+    
+    public func didStartLoadingImageData(for model: FeedImage) {
+        view.display(
+            FeedImageViewModel(
+                location: model.location,
+                description: model.description,
+                image: nil,
+                isLoading: true,
+                shouldRetry: false
+            )
+        )
+    }
+    private struct InvalidImageDataError: Error {}
+
+    public func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        guard let image = imageTransformer(data) else {
+            return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+        }
+
+        view.display(
+            FeedImageViewModel(
+                location: model.location,
+                description: model.description,
+                image: image,
+                isLoading: false,
+                shouldRetry: false
+            )
+        )
+    }
+    
+    public func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+        view.display(
+            FeedImageViewModel(
+                location: model.location,
+                description: model.description,
+                image: nil,
+                isLoading: false,
+                shouldRetry: true
+            )
+        )
+    }
+}
