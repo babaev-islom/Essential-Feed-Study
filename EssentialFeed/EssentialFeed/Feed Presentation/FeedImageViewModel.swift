05faed5fdf9976455312e6518a28d5e341656423
babaev-islom <babaev.iislom@gmail.com>
2022-08-22 13:56:30 +0500

    Remove `FeedImagePresenter` from `EssentialFeediOS` module

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -13,4 +13,8 @@ public struct FeedImageViewModel<Image> {
     public let image: Image?
     public let isLoading: Bool
     public let shouldRetry: Bool
+    
+    public var hasLocation: Bool {
+        location != nil
+    }
 }
