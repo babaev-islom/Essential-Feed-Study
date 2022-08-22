f79e1f62abe82dfbe0e0e36408b0a538f8469855
babaev-islom <babaev.iislom@gmail.com>
2022-08-22 12:08:09 +0500

    Move `FeedErrorViewModel` to separate file

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Islom Babaev on 22/08/22.
+//
+
+import Foundation
+
+struct FeedErrorViewModel {
+    let message: String?
+}
