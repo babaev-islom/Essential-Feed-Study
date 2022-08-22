a8560be53cc09a4b4b50cacb25f8348a5b56ac61
babaev-islom <babaev.iislom@gmail.com>
2022-08-22 12:06:07 +0500

    Show and Hide error message animated

--- EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -8,8 +8,46 @@
 import UIKit
 
 final public class ErrorView: UIView {
-    public var message: String?
+    public var message: String? {
+        get { return isVisible ? button?.title(for: .normal) : nil }
+        set { setMessageAnimated(newValue) }
+    }
     
     @IBOutlet private(set) var button: UIButton?
     
+    public override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        button?.setTitle(nil, for: .normal)
+        alpha = 0
+    }
+    
+    private var isVisible: Bool {
+        return alpha > 0
+    }
+    
+    private func setMessageAnimated(_ message: String?) {
+        if let message = message {
+            showAnimated(message)
+        } else {
+            hideMessageAnimated()
+        }
+    }
+    
+    private func showAnimated(_ message: String) {
+        button?.setTitle(message, for: .normal)
+        
+        UIView.animate(withDuration: 0.25) {
+            self.alpha = 1
+        }
+    }
+    
+    private func hideMessageAnimated() {
+        UIView.animate(
+            withDuration: 0.25,
+            animations: { self.alpha = 0 },
+            completion: { completed in
+                if completed { self.button?.setTitle(nil, for: .normal) }
+            })
+    }
 }
