5060cd9d0a4a5472d55f884716e66277ba16e126
babaev-islom <babaev.iislom@gmail.com>
2022-08-24 18:34:34 +0500

    Simplify production code just to cancel the task (since completion callback should be internally cancelled by each implementation of FeedLoader)

--- EssentialApp/EssentialApp/FeedImageDataLoaderWithFallbackComposite.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderWithFallbackComposite.swift
@@ -18,38 +18,21 @@ public final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader
     }
     
     private final class TaskWrapper: FeedImageDataLoaderTask {
-        private var completion: ((FeedImageDataLoader.Result) -> Void)?
-        
-        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-            self.completion = completion
-        }
-        
         var wrapped: FeedImageDataLoaderTask?
-        
-        func complete(with result: FeedImageDataLoader.Result) {
-            completion?(result)
-        }
         
         func cancel() {
             wrapped?.cancel()
-            preventFurtherCompletions()
-        }
-        
-        private func preventFurtherCompletions() {
-            completion = nil
         }
     }
     
     public func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        let task = TaskWrapper(completion)
+        let task = TaskWrapper()
         task.wrapped = primary.loadImageData(from: url) { [weak self] result in
             switch result {
             case .success:
-                task.complete(with: result)
+                completion(result)
             case .failure:
-                task.wrapped = self?.fallback.loadImageData(from: url) { result in
-                    task.complete(with: result)
-                }
+                task.wrapped = self?.fallback.loadImageData(from: url, completion: completion)
             }
         }
         return task
