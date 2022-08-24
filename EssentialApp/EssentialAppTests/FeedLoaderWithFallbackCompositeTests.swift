fd8ea8d9b75e0baafcf999fa64cc691ebda85e61
babaev-islom <babaev.iislom@gmail.com>
2022-08-24 16:49:04 +0500

    Move `FeedLoaderWithFallbackComposite` to production

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -5,29 +5,10 @@
 //  Created by Islom Babaev on 24/08/22.
 //
 
-import EssentialFeed
-import XCTest
 
-final class FeedLoaderWithFallbackComposite: FeedLoader {
-    private let primary: FeedLoader
-    private let fallback: FeedLoader
-    
-    init(primary: FeedLoader, fallback: FeedLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-    
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load { [weak self] result in
-            switch result {
-            case .success:
-                completion(result)
-            case .failure:
-                self?.fallback.load(completion: completion)
-            }
-        }
-    }
-}
+import XCTest
+import EssentialFeed
+import EssentialApp
 
 final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
     
