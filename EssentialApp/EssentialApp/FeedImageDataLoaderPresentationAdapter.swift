f212b7848d776432c708a0117c0101bf03fa8401
babaev-islom <babaev.iislom@gmail.com>
2022-08-29 16:03:39 +0500

    Replace FeedImageDataLoader composition with Combine operators

--- EssentialApp/EssentialApp/FeedImageDataLoaderPresentationAdapter.swift
+++ EssentialApp/EssentialApp/FeedImageDataLoaderPresentationAdapter.swift
@@ -5,35 +5,41 @@
 //  Created by Islom Babaev on 18/08/22.
 //
 
+import Combine
+import Foundation
 import EssentialFeed
 
 final class FeedImageDataLoaderPresentationAdapter<View: FeedImageView, Image> where View.Image == Image {
     private let model: FeedImage
-    private let imageLoader: FeedImageDataLoader
-    private var task: FeedImageDataLoaderTask?
+    private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
+    private var cancellable: AnyCancellable?
     
     var presenter: FeedImagePresenter<View, Image>?
     
-    init(model: FeedImage, imageLoader: FeedImageDataLoader) {
+    init(model: FeedImage, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) {
         self.model = model
         self.imageLoader = imageLoader
     }
     
     func loadImageData() {
         presenter?.didStartLoadingImageData(for: model)
-        self.task = imageLoader.loadImageData(from: model.url) { [weak self] result in
-            guard let self = self else { return }
-            switch result {
-            case let .success(data):
-                self.presenter?.didFinishLoadingImageData(with: data, for: self.model)
-            case let .failure(error):
-                self.presenter?.didFinishLoadingImageData(with: error, for: self.model)
+        
+        let model = self.model
+        
+        cancellable = imageLoader(model.url)
+            .sink { [weak self] completion in
+                switch completion {
+                case .finished: break
+                case let .failure(error):
+                    self?.presenter?.didFinishLoadingImageData(with: error, for: model)
+                }
+            } receiveValue: { [weak self] data in
+                self?.presenter?.didFinishLoadingImageData(with: data, for: model)
             }
-        }
     }
     
     func cancelImageDataLoad() {
-        task?.cancel()
+        cancellable?.cancel()
     }
     
 }
--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -13,14 +13,21 @@ import EssentialFeediOS
 public final class FeedUIComposer {
     private init() {}
     
-    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: FeedImageDataLoader) -> FeedViewController {
+    public static func feedComposedWith(feedLoader: @escaping () -> FeedLoader.Publisher, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(
             feedLoader: { feedLoader().dispatchOnMainQueue() }
         )
         
         let feedController = makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
         let presenter = FeedPresenter(
-            feedView: FeedViewAdapter(controller: feedController, imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
+            feedView: FeedViewAdapter(
+                controller: feedController,
+                imageLoader: {
+                    imageLoader($0)
+                        .dispatchOnMainQueue()
+                        .eraseToAnyPublisher()
+                }
+            ),
             loadingView: WeakRefVirtualProxy(feedController),
             errorView: WeakRefVirtualProxy(feedController)
         )
