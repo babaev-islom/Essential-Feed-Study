fc7ca621b811d0960192d2cb6e91cb5d176d3b19
babaev-islom <babaev.iislom@gmail.com>
2022-10-17 13:43:57 +0500

    Prevent loading resources again until a running request completes

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -12,7 +12,7 @@ import EssentialFeediOS
 final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let loader: () -> AnyPublisher<Resource, Error>
     private var cancellable: AnyCancellable?
-    
+    private var isLoading = false
     var presenter: LoadResourcePresenter<Resource, View>?
     
     init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
@@ -20,20 +20,29 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
     
     func loadResource() {
-        presenter?.didStartLoading()
+        guard !isLoading else { return }
         
-        cancellable = loader().sink(
-            receiveCompletion: { [weak self] completion in
-                switch completion {
-                case .finished: break
-                    
-                case let .failure(error):
-                    self?.presenter?.didFinishLoading(with: error)
-                }
-            },
-            receiveValue: { [weak self] resource in
-                self?.presenter?.didFinishLoading(with: resource)
+        presenter?.didStartLoading()
+        isLoading = true
+        
+        cancellable = loader()
+            .handleEvents(receiveCancel: { [weak self] in
+                self?.isLoading = false
             })
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    switch completion {
+                    case .finished: break
+                        
+                    case let .failure(error):
+                        self?.presenter?.didFinishLoading(with: error)
+                    }
+                    self?.isLoading = false
+                },
+                receiveValue: { [weak self] resource in
+                    self?.presenter?.didFinishLoading(with: resource)
+                }
+            )
     }
 }
 
